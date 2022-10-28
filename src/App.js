1fc53ef4fc9ae832ec557cff1ffadfb28ad483f1
Md. Habibur Rahman Shohel <md.hrshohel@gmail.com>
2022-10-29 03:58:34 +0600

    input field added

--- src/App.js
+++ src/App.js
@@ -11,9 +11,27 @@ function App() {
       .then((data) => setUsers(data));
   }, [])
 
+  const handleAddUser = (e) => {
+    e.preventDefault();
+    const name = e.target.name.value;
+    const email = e.target.email.value;
+    const newUser = { name, email };
+    console.log(newUser);
+  }
+
   return (
     <div className="App">
-      <h1>Users: {users.length}</h1>
+
+      <form onSubmit={handleAddUser}>
+        <input type="text" name='name' placeholder="Enter your name" />
+        <br />
+        <input type="email" name='email' placeholder="Enter your email" />
+        <br />
+        <button type="submit">Add User</button>
+      </form>
+
+
+      <h2>Users: {users.length}</h2>
       <div>
         {
           users.map(user => <p key={user.id}>{user.name} {user.email}</p>)
